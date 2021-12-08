8be70b9a64e5bca245e949d7c8d86b9d3e5ffa28
turtore <brunosap@gmail.com>
2021-12-08 20:42:12 -0300

    atende requisito 1

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+// entradas
+const fs = require('fs');
+
+const talkerFile = 'talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// requisito 1
+app.get('/talker', (req, res) => {
+   fs.readFile(talkerFile, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Não foi possível ler o arquivo ${talkerFile}\n Erro: ${err}`);
+        process.exit(1);
+      }
+      res.status(200).send(data);    
+    });
 });
 
 app.listen(PORT, () => {
